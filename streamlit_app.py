38d6fd844a4ecd96f62478573cf2ce6db20f91b6
Subhashini Natarajan <subhashini.natarajan@joinhonor.com>
2024-08-26 21:57:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,12 +19,8 @@ bucket_name = 'h4-hack-week-aug-2024'
 object_key = 'myfile.csv'
 path = 's3://{}:{}@{}/{}'.format("AKIA5DRNUTKHYRXM5LPV", "KOU2S7VqQQmB4974Vl5Ve0CRMxBPZ55RwR0HFM1O", bucket_name, object_key)
 
-
-#Connecting to AWS through smart_open python package and getting the data
-@st.experimental_memo
-def load_data(path):
-    data = pd.read_csv(smart_open(path),index_col=0)
-    return data
-
-df = load_data(path) #saving the returned data into dataframe
+data = pd.read_csv(smart_open(path),index_col=0)
 st.write(df)
+
+
+
