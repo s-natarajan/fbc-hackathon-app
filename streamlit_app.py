b8c2f9a42b773e5c425438305df62d82caaa3393
Subhashini Natarajan <subhashini.natarajan@joinhonor.com>
2024-08-26 21:44:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+from st_files_connection import FilesConnection
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
@@ -7,3 +9,11 @@ st.write(
 st.write(
     "Hello World!"
 )
+# Create connection object and retrieve file contents.
+# Specify input format is a csv and to cache the result for 600 seconds.
+conn = st.connection('s3', type=FilesConnection)
+df = conn.read("testbucket-jrieke/myfile.csv", input_format="csv", ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.Owner} has a :{row.Pet}:")
