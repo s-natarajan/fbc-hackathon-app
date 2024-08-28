d2ef481c3ee085d94b4fbf681bc057cb6406176c
Subhashini Natarajan <subhashini.natarajan@joinhonor.com>
2024-08-27 22:30:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ def generate_slide_content(topic, content):
     #st.write(f"{row}")
     
     prompt = f"Generate slide ideas for {topic}:\n\n{df.to_string()}"
-    prompt_txt = f"You are a helpful assistant that generates an executive summary of Franchise performance metrics. For Franchise number: {topic} return summary of details including First Name & Last Name as Franchisee, NetworkPartnerName as FBC, State as DO."
+    prompt_txt = f"You are a helpful assistant that generates an executive summary of Franchise performance metrics. For Franchise number: {topic} return the details limited to First Name & Last Name as Franchisee, NetworkPartnerName as FBC, State as DO, Weighted Score and Rank."
 
     # Use ChatCompletion with the new model and API method
     response = openai.chat.completions.create(
