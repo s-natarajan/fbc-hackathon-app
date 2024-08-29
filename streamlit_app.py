692b02b078c4c67fd8a17e2912ef1a748fba0d30
Subhashini Natarajan <subhashini.natarajan@joinhonor.com>
2024-08-29 18:07:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ def generate_slide_content(content):
     )
     generated_text = response.choices[0].message.content
     generated_text = generated_text.removeprefix("```python")
-    st.write(isinstanceof(generated_text, dict))
+    st.write(isinstance(generated_text, dict))
     st.write(f"Response: {generated_text}")
     return ast.literal_eval(generated_text)
 
