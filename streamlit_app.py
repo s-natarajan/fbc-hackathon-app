b6907a2c26cc72ceaa85d3ba2d01a6a65dad12d4
Subhashini Natarajan <subhashini.natarajan@joinhonor.com>
2024-08-29 15:44:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,16 +34,13 @@ def get_franchise_data(topic):
     keys_to_keep = [key.strip() for key in topic.split(',')]
     st.write(keys_to_keep)
     filtered_dict = {}
-    #st.write(df.items())
     for data in df:
-        st.write(data)
         if str(data) in keys_to_keep:
             st.write("should come here multiple times")
             #st.write(data)
             #st.write(df.get(data))
             #st.write(df[data])
             filtered_dict[str(data)] = df[data]
-    st.write("filtered dict")
     return filtered_dict
     
 # Function to generate slide content
