94f486f4b74fe265173070bf33f16ed06ceccfdd
Subhashini Natarajan <subhashini.natarajan@joinhonor.com>
2024-08-30 00:56:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -165,7 +165,7 @@ def create_presentation(franchise_data, slide_content, key_insights):
             performance_standing = "Significantly Above Target"
         #perf_shape.text = performance_standing
         for placeholder in shapes.placeholders:
-            if placeholder.name == 'Text Placeholder 3':
+            if placeholder.name == 'Title 2':
                 placeholder.text = performance_standing
                 
             st.write(placeholder.name)
