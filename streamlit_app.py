15e2475757f516c120cfb437b058ce22abf04da2
Subhashini Natarajan <subhashini.natarajan@joinhonor.com>
2024-08-30 00:44:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -139,7 +139,7 @@ def create_presentation(franchise_data, slide_content, key_insights):
         slide = prs.slides.add_slide(bullet_slide_layout)
         shapes = slide.shapes
         title_shape = shapes.title
-        st.write(shapes.placeholders)
+        st.write(shapes.placeholders.len())
         body_shape = shapes.placeholders[1]
         perf_shape = shapes.placeholders[2]
         tf = body_shape.text_frame
